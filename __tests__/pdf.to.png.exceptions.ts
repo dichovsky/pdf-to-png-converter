37fc8dbdeead55195813f56bcf7ee74777dc26ca
DCampagnola <dcampagnola.dev@gmail.com>
2021-12-22 20:04:57 +0100

    Added buffer as input

--- __tests__/pdf.to.png.exceptions.ts
+++ __tests__/pdf.to.png.exceptions.ts
@@ -1,8 +1,15 @@
 import { resolve } from 'path';
 import { pdfToPng } from '../src';
+import {readFileSync} from "fs";
 
 test(`Should throw "PDF file not found" exception`, async () => {
     const pdfFilePath: string = resolve('non_existing_file.pdf');
     
     await expect(async() => { await pdfToPng(pdfFilePath) }).rejects.toThrow(Error);
+});
+
+test(`Should throw "outputFileMask is required when input is a Buffer" exception`, async () => {
+    const pdfFileBuffer: Buffer = readFileSync(resolve('test-data/large_pdf.pdf'));
+
+    await expect(async() => { await pdfToPng(pdfFileBuffer) }).rejects.toThrow(Error);
 });
