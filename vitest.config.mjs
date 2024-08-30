4e8527d4c9e2b3fde5fbb3d2213031d7120ad19e
Igor Magdich <magdich.igor@gmail.com>
2024-08-30 22:02:22 +0300

    Update test node to 18 20 22

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    testTimeout: 60000,
+    testTimeout: 90000,
     coverage: {
       provider: 'v8'
     },
