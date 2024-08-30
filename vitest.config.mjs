3d486001ccdfc47ef29dd9cb5493ec556e6fc46a
Igor Magdich <magdich.igor@gmail.com>
2024-08-30 17:15:33 +0300

    Extend test timeout to 60000

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    testTimeout: 30000,
+    testTimeout: 60000,
     coverage: {
       provider: 'v8'
     },
